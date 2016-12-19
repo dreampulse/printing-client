2e14a5977526fcf39556da2f1e76add0d70dca21
Tankred Hase <mail@tankredhase.de>
2016-12-19 16:49:22 +0100

    Add check location attributes in test

--- test/integration/app/service/geolocation.spec.js
+++ test/integration/app/service/geolocation.spec.js
@@ -4,7 +4,10 @@ describe('Geolocation Integration Test', () => {
   describe('getLocation()', () => {
     it('should work', async () => {
       const location = await getLocation()
+      expect(location.city, 'to be ok')
       expect(location.zipCode, 'to be ok')
+      expect(location.stateCode, 'to be ok')
+      expect(location.countryCode, 'to be ok')
     })
   })
 })
