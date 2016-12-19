bd54be7ca65480ff806fd4586cf72223f7ae0c9f
Tankred Hase <mail@tankredhase.de>
2016-12-19 16:51:36 +0100

    Increase timeout in geolocation test

--- test/integration/app/service/geolocation.spec.js
+++ test/integration/app/service/geolocation.spec.js
@@ -1,6 +1,8 @@
 import { getLocation } from '../../../../src/app/service/geolocation'
 
-describe('Geolocation Integration Test', () => {
+describe('Geolocation Integration Test', function () {
+  this.timeout(20000)
+
   describe('getLocation()', () => {
     it('should work', async () => {
       const location = await getLocation()
