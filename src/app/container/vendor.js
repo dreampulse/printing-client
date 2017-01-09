78354c17f620b178204f9f5d2ed0e0dd55496b4b
Tankred Hase <mail@tankredhase.de>
2017-01-09 15:36:34 +0100

    Use spread operator instead of apply

--- src/app/container/vendor.js
+++ src/app/container/vendor.js
@@ -81,5 +81,4 @@ const enhance = compose(
   })
 )
 
-
 export default enhance(Vendor)
--- src/app/lib/get-total-amount.js
+++ src/app/lib/get-total-amount.js
@@ -6,6 +6,6 @@ export function getCartAmount (cart) {
 
 export function getPriceAmount (vendor) {
   const itemPrice = vendor.items.reduce((last, cur) => last + cur.price, 0)
-  const shippingPrice = Math.min.apply(null, vendor.shipping.map(s => s.price))
+  const shippingPrice = Math.min(...vendor.shipping.map(s => s.price))
   return Math.round((itemPrice + shippingPrice) * (vendor.vatPercentage + 1) * 100) / 100
 }
