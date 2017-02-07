61ef7054209a485d2a6224daf2b9959c9904c1e0
Mario Volke <mv@codastic.com>
2017-02-07 10:52:11 +0100

    Remove coverage from npm run test

--- test/integration/app/service/geolocation.spec.js
+++ test/integration/app/service/geolocation.spec.js
@@ -1,6 +1,6 @@
 import {getLocation} from '../../../../src/app/service/geolocation'
 
-describe('Geolocation Integration Test', () => {
+describe('Geolocation Integration Test', function test () {
   this.timeout(20000)
 
   describe('getLocation()', () => {
