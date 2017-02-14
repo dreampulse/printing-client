708fb989cc7310ea237cb8aca30a297f3a263cad
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-02-14 16:42:27 +0100

    Fix review issues

--- src/app/action/init.js
+++ src/app/action/init.js
@@ -2,8 +2,7 @@ import {detectAddress, createUser} from './user'
 import {getMaterials} from './material'
 
 export default () => async (dispatch) => {
-  dispatch(getMaterials())
-
+  await dispatch(getMaterials())
   await dispatch(detectAddress())
-  dispatch(createUser())
+  await dispatch(createUser())
 }
