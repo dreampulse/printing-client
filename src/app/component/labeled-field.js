af4236099057773584c5b6c0d536928995fd911c
Chris Ranz <git@christianranz.com>
2017-02-17 15:05:24 +0100

    Add labeled field with redux-form api

---
+++ src/app/component/labeled-field.js
@@ -0,0 +1,45 @@
+import React, {cloneElement} from 'react'
+import uniqueId from 'lodash/uniqueId'
+
+import propTypes from '../lib/prop-types'
+import buildClassName from '../lib/build-class-name'
+
+const LabeledField = ({
+  classNames,
+  modifiers = [],
+  label,
+  children,
+  name,
+  onChange,
+  value
+}) => {
+  const id = uniqueId('uid-')
+  const mappedChildren = React.Children.map(children, child => (
+    cloneElement(child, {
+      name: child.props.name || name,
+      onChange: child.props.onChange || onChange,
+      value: child.props.value || value,
+      id
+    })
+  ))
+  return (
+    <div className={buildClassName('labeled-field', modifiers, classNames)}>
+      <label htmlFor={id} className="labeled-field__label">{label}</label>
+      <div className="labeled-field__field">{mappedChildren}</div>
+    </div>
+  )
+}
+
+LabeledField.propTypes = {
+  ...propTypes.component,
+  label: React.PropTypes.string.isRequired,
+  children: React.PropTypes.node.isRequired,
+  value: React.PropTypes.oneOfType([
+    React.PropTypes.string.isRequired,
+    React.PropTypes.number.isRequired
+  ]),
+  name: React.PropTypes.string,
+  onChange: React.PropTypes.func
+}
+
+export default LabeledField
---
+++ stories/component/labeled-field.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {storiesOf} from '@kadira/storybook'
+
+import LabeledField from '../../src/app/component/labeled-field'
+import NumberField from '../../src/app/component/number-field'
+import HandleValue from '../util/handle-value'
+
+storiesOf('Labeled Field', module)
+  .add('default', () => (
+    <HandleValue initialValue={0}>
+      <LabeledField label="Label:">
+        <NumberField />
+      </LabeledField>
+    </HandleValue>
+  ))
