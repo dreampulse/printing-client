4c14bad4380f6c4de1a7ebd6b6c368059805b8eb
Chris Ranz <git@christianranz.com>
2017-02-17 12:20:01 +0100

    Add bottom margin to form-row and remove from fields

--- src/app/component/form-row.js
+++ src/app/component/form-row.js
@@ -3,11 +3,17 @@ import React, {PropTypes} from 'react'
 import propTypes from '../lib/prop-types'
 import buildClassName from '../lib/build-class-name'
 
-const FormRow = ({classNames, modifiers, children}) => (
-  <div className={buildClassName('form-row', modifiers, classNames)}>
-    {children}
-  </div>
-)
+const FormRow = ({classNames, modifiers, children}) => {
+  const mappedChildren = React.Children.map(children, child => (
+    <div className="form-row__column">{child}</div>
+  ))
+
+  return (
+    <div className={buildClassName('form-row', modifiers, classNames)}>
+      {mappedChildren}
+    </div>
+  )
+}
 
 FormRow.propTypes = {
   ...propTypes.component,
