69b76074c855a3f531e583da12c78202cc6891fd
Mario Volke <mv@codastic.com>
2017-02-28 09:37:00 +0100

    Streamline webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,16 @@
-const webpackDevelopConfig = require('../webpack.develop.config')
+const webpackConfig = require('../webpack/config')
+
+const config = webpackConfig({
+  optimize: false,
+  nodeEnv: 'development'
+})
 
 // Remove JS loader to use default provided by Storybook
-const loaders = webpackDevelopConfig.module.loaders.filter(loader => !(loader.loaders && loader.loaders.indexOf('babel') >= 0))
+const loaders = config.module.loaders.filter(loader => !(loader.loaders && loader.loaders.indexOf('babel') >= 0))
 
 module.exports = {
   module: {
     loaders
   },
-  postcss: webpackDevelopConfig.postcss
+  postcss: config.postcss
 }
