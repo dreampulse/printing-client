702c7ab5e3b65cf39164a292e3444c033bf00aaa
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-03-02 10:30:39 +0100

    Fix geolocation issue

--- src/app/action/init.js
+++ src/app/action/init.js
@@ -2,7 +2,7 @@ import {detectAddress, createUser} from './user'
 import {getMaterials} from './material'
 
 export default () => async (dispatch) => {
-  await dispatch(getMaterials())
-  await dispatch(detectAddress())
-  await dispatch(createUser())
+  dispatch(getMaterials())
+  dispatch(detectAddress())
+  dispatch(createUser())
 }
