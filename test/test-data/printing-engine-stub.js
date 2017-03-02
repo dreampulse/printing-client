a2f0cd336e223ca031495c50354bf82effc440e9
Tankred Hase <mail@tankredhase.de>
2017-03-02 14:22:26 +0100

    Complete printing engine stub dummy responses

--- test/test-data/printing-engine-stub.js
+++ test/test-data/printing-engine-stub.js
@@ -1,14 +1,16 @@
+import cartResponse from './cart-response.json'
+import priceResponse from './price-response.json'
 import materials from './generated-material-structure.json'
 import * as printingEngine from '../../src/app/lib/printing-engine'
 
-printingEngine.uploadModel = () => Promise.resolve({modelId: '5a969cea-fa19-47a8-9a24-5cde671e0609'})
+printingEngine.uploadModel = () => Promise.resolve({modelId: 'b364bd10-6d64-4515-b832-495d0fdf7d4c'})
 printingEngine.getUploadStatus = () => Promise.resolve(true)
 printingEngine.listMaterials = () => Promise.resolve(materials)
-printingEngine.createUser = () => Promise.resolve({userId: '009d58b7-a33d-4354-9eaa-1b6f408919d3'})
+printingEngine.createUser = () => Promise.resolve({userId: 'c6807f8c-a1a1-44ed-85cf-9f34784e8d9f'})
 printingEngine.updateUser = () => Promise.resolve()
-printingEngine.createPriceRequest = () => Promise.resolve({priceId: '9f4f7685-185a-48de-8e5b-fdf497c9acf7'})
+printingEngine.createPriceRequest = () => Promise.resolve({priceId: '2baf30a1-3208-4f4f-bbb2-8f7b4fdccc7e'})
 printingEngine.getPriceStatus = () => Promise.resolve(true)
-printingEngine.getPrice = () => Promise.resolve({})
-printingEngine.createShoppingCart = () => Promise.resolve({cartId: '5a969cea-fa19-47a8-9a24-5cde671e0606'})
-printingEngine.getFinalCartPrice = () => Promise.resolve({})
+printingEngine.getPrice = () => Promise.resolve(priceResponse)
+printingEngine.createShoppingCart = () => Promise.resolve({cartId: '2e7e6852-0c4f-43af-980a-427bbb0dae05'})
+printingEngine.getFinalCartPrice = () => Promise.resolve(cartResponse)
 printingEngine.order = () => Promise.resolve({orderId: '6f4f7685-185a-48de-8e5b-fdf497c9acf7'})
