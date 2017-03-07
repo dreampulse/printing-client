b1dd3292ad5319a63bedbf6bfe94df82c501d5ca
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-03-07 18:20:35 +0100

    Add working overlay

--- src/app/container/modal/index.js
+++ src/app/container/modal/index.js
@@ -4,25 +4,15 @@ import Portal from 'react-portal'
 
 import {close} from '../../action/modal'
 
-// import Portal from '../../component-legacy/portal'
-// import ModalComponent from '../../component-legacy/modal'
-
 const Modal = ({title, isOpen, onModalClose, contentFactory, contentProps, contentModifiers}) => (
   <Portal
     closeOnEsc
-    closeOnOutsideClick
     isOpened={isOpen}
     onClose={onModalClose}
   >
     {createElement(contentFactory || 'div', contentProps)}
   </Portal>
 )
-
-  // <Portal modifiers={['modal', ...contentModifiers]} isOpen={isOpen} onRequestClose={onModalClose}>
-  //   <ModalComponent title={title} onClose={onModalClose}>
-  //     {createElement(contentFactory || 'div', contentProps)}
-  //   </ModalComponent>
-  // </Portal>
 
 const mapStateToProps = state => ({
   isOpen: state.modal.isOpen,
