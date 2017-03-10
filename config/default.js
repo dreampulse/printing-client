995d06cdf2953f632884a69b2d0a2d2d74facca9
Mario Volke <mv@codastic.com>
2017-03-10 13:41:17 +0100

    Fix config

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 export default {
   pollingRetries: 100,
-  pollingInvervall: 1000,
+  pollingInverval: 1000,
   imagePollingRetries: 5,
   imagePollingInterval: 1000,
   cloudinaryUrl: '//res.cloudinary.com/all3dp/image/upload',
