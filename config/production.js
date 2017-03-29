265be798349ebff9739c03de42d301eeaa67dd5a
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-03-29 13:40:45 +0200

    Update production config

--- config/production.js
+++ config/production.js
@@ -3,5 +3,6 @@ import defaultConfig from './default'
 
 export default defaultsDeep({
   printingEngineBaseUrl: 'https://printing-engine.all3dp.com/v1',
-  googleMapsApiKey: 'TODO'
+  stripePublicKey: 'pk_test_vgy9WdRy48FnhegkMYXMQXit',  // Key for the test System
+  googleMapsApiKey: 'AIzaSyC_f-WiODvaWW-Hw5T9RZRSKkvLcy9WCGU'
 }, defaultConfig)
