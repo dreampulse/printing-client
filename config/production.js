a68575c71d73cbe1d0a3a73a8df50a0c95fe3edb
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-04-20 16:30:25 +0200

    Add production settings for stripe

--- config/production.js
+++ config/production.js
@@ -3,6 +3,6 @@ import defaultConfig from './default'
 
 export default defaultsDeep({
   printingEngineBaseUrl: 'https://printing-engine.all3dp.com/v1',
-  stripePublicKey: 'pk_test_vgy9WdRy48FnhegkMYXMQXit',  // Key for the test System
+  stripePublicKey: 'pk_live_4OJl78TMOSPW8HHxzml8Xfr5',
   googleMapsApiKey: 'AIzaSyC_f-WiODvaWW-Hw5T9RZRSKkvLcy9WCGU'
 }, defaultConfig)
