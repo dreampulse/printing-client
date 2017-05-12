beee1b63280687f948570673f86f46042ee55138
Tankred Hase <mail@tankredhase.de>
2017-05-12 10:35:16 +0200

    Add paypal production credentials to config

--- config/production.js
+++ config/production.js
@@ -4,5 +4,12 @@ import defaultConfig from './default'
 export default defaultsDeep({
   printingEngineBaseUrl: 'https://printing-engine.all3dp.com/v1',
   stripePublicKey: 'pk_live_4OJl78TMOSPW8HHxzml8Xfr5',
+  paypal: {
+    locale: 'en_US',
+    env: 'production',
+    client: {
+      production: 'AbvitbyqkRJcWOjzsxRH-W__HaSJcbweV96ifdHOonh94toIyopK8hlPUmrkJmArWuR0f4ggKMpzRgZP'
+    }
+  },
   googleMapsApiKey: 'AIzaSyC_f-WiODvaWW-Hw5T9RZRSKkvLcy9WCGU'
 }, defaultConfig)
