52d79f3061d1fffdc66f5712ef8ed6ab6350f570
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-07-05 16:54:47 +0200

    Add tracking for people in mixpanel

--- src/app/service/mixpanel.js
+++ src/app/service/mixpanel.js
@@ -7,3 +7,7 @@ export const track = (...params) => {
 export const identify = (...params) => {
   if (mixpanel && mixpanel.identify) mixpanel.identify(...params)
 }
+
+export const peopleSet = (...params) => {
+  if (mixpanel && mixpanel.people && mixpanel.people.set) mixpanel.people.set(...params)
+}
