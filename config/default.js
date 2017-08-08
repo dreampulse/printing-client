73a79a43bfaa5e54c7fe028f500a165c75ad087d
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-08-08 13:31:22 +0200

    Fix default material

--- config/default.js
+++ config/default.js
@@ -9,7 +9,7 @@ export default {
   cloudinaryDefaultParams: ['q_90', 'f_auto'],
   scrollToOffset: 100, // Height of sticky header in pixels plus spacing
   debouncePriceRequestWait: 1000,
-  defaultSelectedMaterial: 'Polyamide',
+  defaultSelectedMaterial: 'Premium Plastic',
   stripeCheckoutImage: 'asset/image/checkout-logo.png',
   raven: 'https://ea87ef20371a4316aca7c9f415aad1f9@sentry.io/193367'
 }
