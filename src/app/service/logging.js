7f99c005cd2cf2b37172ed73c8a68c86e1f3a2cb
Tankred Hase <mail@tankredhase.de>
2017-10-02 13:28:44 +0200

    Fix typo in logging service

--- src/app/service/logging.js
+++ src/app/service/logging.js
@@ -6,5 +6,5 @@ if (process.env.NODE_ENV === 'production') {
   Raven.config(config.raven).install()
 }
 
-export const captureException = execption => Raven && Raven.captureException(execption)
+export const captureException = exception => Raven && Raven.captureException(exception)
 export const setUserContext = user => Raven && Raven.setUserContext(user)
