fff24a76d752921cfb44640fed3d624d3ae309f3
Jonathan Häberle <jonathan.haeberle@gmail.com>
2017-11-06 10:46:12 +0100

    Support multible fileformats, resolves #371

--- stories/component/upload-area.js
+++ stories/component/upload-area.js
@@ -6,9 +6,10 @@ import UploadArea from 'Component/upload-area'
 
 storiesOf('Upload Area', module).add('default', () => (
   <UploadArea
-    label="Drag 3D files here or"
+    label="Drag any 3D files here or"
     linkLabel="select files"
-    accept=".stl"
+    subLine="Supported file formats: STL, OBJ, WRL, SKP, DAE, 3DS, IGS, FBX, PLY, X3D, STP, PRT, …"
+    accept="*.*"
     onChange={action('change')}
   />
 ))
