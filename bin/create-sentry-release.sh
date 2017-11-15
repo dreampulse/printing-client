8b8762273214c371b67095b7a29e3dc63484de70
Johannes Ewald <johannes.ewald@peerigon.com>
2017-11-15 16:19:48 +0100

    Fix shellcheck warnings

--- bin/create-sentry-release.sh
+++ bin/create-sentry-release.sh
@@ -4,15 +4,15 @@
 set -eu
 
 # SENTRY_AUTH_TOKEN must be set externally
+# exported env variables are used by the sentry-cli
 export SENTRY_ORG="all3dp-gmbh"
 export SENTRY_PROJECT="printing-engine-client"
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIST_PATH="./dist"
-SOURCE_MAPS=$(find $DIST_PATH -name '*.js.map' -type f)
-VERSION=$( $SCRIPT_PATH/get-sentry-release-version.sh )
+VERSION=$( "$SCRIPT_PATH"/get-sentry-release-version.sh )
 
-npx sentry-cli releases new $VERSION
-npx sentry-cli releases set-commits --auto $VERSION
-npx sentry-cli releases files $VERSION upload-sourcemaps --rewrite --ignore-file $SCRIPT_PATH/.sentryignore $DIST_PATH
-npx sentry-cli releases finalize $VERSION
-npx sentry-cli releases deploys $VERSION new -e production
+npx sentry-cli releases new "$VERSION"
+npx sentry-cli releases set-commits --auto "$VERSION"
+npx sentry-cli releases files "$VERSION" upload-sourcemaps --rewrite --ignore-file "$SCRIPT_PATH"/.sentryignore "$DIST_PATH"
+npx sentry-cli releases finalize "$VERSION"
+npx sentry-cli releases deploys "$VERSION" new -e production
