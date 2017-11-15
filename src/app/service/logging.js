fe59d3fa900340f29e414295dde135009e3ef467
Johannes Ewald <johannes.ewald@peerigon.com>
2017-11-15 16:10:09 +0100

    Add environment property to raven config

--- src/app/service/logging.js
+++ src/app/service/logging.js
@@ -5,7 +5,8 @@ import config from '../../../config'
 
 if (process.env.NODE_ENV === 'production') {
   Raven.config(config.ravenUrl, {
-    release: config.ravenRelease
+    release: config.ravenRelease,
+    environment: process.env.NODE_ENV
   }).install()
 }
 
