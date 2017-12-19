aab55cad88c6b9f6174e456390c20d25e9adc2ca
Johannes Ewald <johannes.ewald@peerigon.com>
2017-12-19 12:12:48 +0100

    Add missing tests for getUtmParams()

--- test/unit/app/lib/search-params.spec.js
+++ test/unit/app/lib/search-params.spec.js
@@ -1,6 +1,6 @@
 import URLSearchParams from 'url-search-params'
 import * as searchParams from 'App/service/search-params'
-import {getSearchParams} from 'App/lib/search-params'
+import {getSearchParams, getUtmParams} from 'App/lib/search-params'
 
 describe('Search Params Lib', () => {
   let sandbox
@@ -13,7 +13,7 @@ describe('Search Params Lib', () => {
     sandbox.restore()
   })
 
-  describe('getSearchParams', () => {
+  describe('getSearchParams()', () => {
     it('returns an empty object when there are no search params', () => {
       const urlSearchParams = new URLSearchParams()
 
@@ -32,6 +32,46 @@ describe('Search Params Lib', () => {
       expect(getSearchParams(), 'to equal', {
         a: 'some string',
         b: 'some other string'
+      })
+    })
+  })
+
+  describe('getUtmParams()', () => {
+    it('returns an empty object when there are no UTM params', () => {
+      const urlSearchParams = new URLSearchParams()
+
+      searchParams.fromLocationSearch.returns(urlSearchParams)
+
+      expect(getUtmParams(), 'to equal', {})
+    })
+
+    it('returns an object with the defined UTM params', () => {
+      const urlSearchParams = new URLSearchParams()
+
+      urlSearchParams.set('utm_source', 'some source')
+      searchParams.fromLocationSearch.returns(urlSearchParams)
+
+      expect(getUtmParams(), 'to equal', {
+        source: 'some source'
+      })
+    })
+
+    it('recognizes all five UTM params', () => {
+      const urlSearchParams = new URLSearchParams()
+
+      urlSearchParams.set('utm_source', 'some source')
+      urlSearchParams.set('utm_medium', 'some medium')
+      urlSearchParams.set('utm_campaign', 'some campaign')
+      urlSearchParams.set('utm_term', 'some term')
+      urlSearchParams.set('utm_content', 'some content')
+      searchParams.fromLocationSearch.returns(urlSearchParams)
+
+      expect(getUtmParams(), 'to equal', {
+        source: 'some source',
+        medium: 'some medium',
+        campaign: 'some campaign',
+        term: 'some term',
+        content: 'some content'
       })
     })
   })
