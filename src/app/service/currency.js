7d6d63d5fefde94aecf94db83ca5d1c67a506896
Henry Pötzl <henry@all3dp.com>
2017-12-20 16:43:40 +0100

    Provide static list of currencies

--- src/app/service/currency.js
+++ src/app/service/currency.js
@@ -1,28 +1,11 @@
-import {getCurrencyList, getCurrencyAbbreviation} from 'country-currency-map'
-import {getCountryName} from './country'
-
-const mapCurrencyToSelectModel = currency => ({value: currency.abbr, label: currency.abbr})
-
-const sortValuesAlphabetically = (a, b) => {
-  const valueA = a.value.toUpperCase()
-  const valueB = b.value.toUpperCase()
-  if (valueA < valueB) {
-    return -1
-  } else if (valueA > valueB) {
-    return 1
-  }
-  return 0
-}
-
-const currencies = getCurrencyList()
-  .map(mapCurrencyToSelectModel)
-  .sort(sortValuesAlphabetically)
+const currencies = [
+  {value: 'USD', label: 'USD'},
+  {value: 'EUR', label: 'EUR'},
+  {value: 'GBP', label: 'GBP'},
+  {value: 'CAD', label: 'CAD'},
+  {value: 'AUD', label: 'AUD'}
+]
 
 export function getCurrencies() {
   return currencies
-}
-
-export function getCurrencyForCountry(countryCode) {
-  const currency = getCurrencyAbbreviation(getCountryName(countryCode))
-  return currency || 'USD'
 }
