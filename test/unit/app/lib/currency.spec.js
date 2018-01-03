b322c02040b803c0fce018b9f216f1885d389552
Henry Pötzl <henry@all3dp.com>
2018-01-03 15:33:26 +0100

    Refactor currency lib unit tests

--- test/unit/app/lib/currency.spec.js
+++ test/unit/app/lib/currency.spec.js
@@ -1,26 +1,34 @@
 import * as currency from '../../../../src/app/lib/currency'
 
 describe('Currency lib', () => {
-  describe('getCurrencies', () => {
-    it('should return currencies', () => {
-      const currencies = currency.getCurrencies()
+  describe('getCurrencies()', () => {
+    let currencies
+    beforeEach(() => {
+      currencies = currency.getCurrencies()
+    })
+    it('returns all currencies supported', () => {
       expect(currencies.length, 'to equal', 3)
+    })
+    it('returns currencies prepared for select menu', () => {
       expect(currencies, 'to have items satisfying', 'to have own properties', ['value', 'label'])
     })
   })
 
-  describe('formatCurrency', () => {
-    it('should return formatted price for known currencies', () => {
-      const value = '10.00'
+  describe('formatCurrency()', () => {
+    const value = '10.00'
+    it('returns formatted price for USD', () => {
       const priceUsd = currency.formatCurrency(value, 'USD')
-      const priceEur = currency.formatCurrency(value, 'EUR')
-      const priceGbp = currency.formatCurrency(value, 'GBP')
       expect(priceUsd, 'to equal', '$10.00')
+    })
+    it('returns formatted price for EUR', () => {
+      const priceEur = currency.formatCurrency(value, 'EUR')
       expect(priceEur, 'to equal', '€10.00')
+    })
+    it('returns formatted price for GBP', () => {
+      const priceGbp = currency.formatCurrency(value, 'GBP')
       expect(priceGbp, 'to equal', '£10.00')
     })
-
-    it('should return formatted price for unknown currency', () => {
+    it('returns formatted price for unknown currency', () => {
       const price = currency.formatCurrency('10.00', 'AUD')
       expect(price, 'to equal', 'AUD10.00')
     })
