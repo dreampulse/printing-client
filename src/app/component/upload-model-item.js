cb8db33f360a6163527d6239dce50df14cee2822
Mario Volke <mv@codastic.com>
2018-01-24 11:48:44 +0100

    Add button bar to upload model item

--- src/app/component/upload-model-item.js
+++ src/app/component/upload-model-item.js
@@ -1,28 +1,57 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
 
 import propTypes from '../lib/prop-types'
 import buildClassName from '../lib/build-class-name'
 
 import ImageContainer from './image-container'
 
-const UploadModelItem = ({classNames, modifiers, imageSource, title, subline, quantity}) => (
-  <div className={buildClassName('upload-model-item', modifiers, classNames)}>
-    <ImageContainer source={imageSource} />
-    <div className="upload-model-item__content">
-      {Boolean(title) && <strong className="upload-model-item__title">{title}</strong>}
-      {Boolean(subline) && <span className="upload-model-item__subline">{subline}</span>}
-      {Boolean(quantity) && <span className="upload-model-item__quantity">Qty: {quantity}</span>}
+const UploadModelItem = ({
+  classNames,
+  modifiers = [],
+  imageSource,
+  title,
+  subline,
+  quantity,
+  isButtonBarVisible,
+  setIsButtonBarVisible,
+  buttonBar
+}) => {
+  const finalModifiers = [
+    ...modifiers,
+    {
+      'show-buttons': isButtonBarVisible
+    }
+  ]
+
+  return (
+    <div
+      className={buildClassName('upload-model-item', finalModifiers, classNames)}
+      onMouseEnter={() => setIsButtonBarVisible(true)}
+      onMouseLeave={() => setIsButtonBarVisible(false)}
+    >
+      <ImageContainer source={imageSource} />
+      <div className="upload-model-item__content">
+        {Boolean(title) && <strong className="upload-model-item__title">{title}</strong>}
+        {Boolean(subline) && <span className="upload-model-item__subline">{subline}</span>}
+        {Boolean(quantity) && <span className="upload-model-item__quantity">Qty: {quantity}</span>}
+      </div>
+      {buttonBar && <div className="upload-model-item__buttons">{buttonBar}</div>}
     </div>
-  </div>
-)
+  )
+}
 
 UploadModelItem.propTypes = {
   ...propTypes.component,
   imageSource: PropTypes.string.isRequired,
   title: PropTypes.string,
   subline: PropTypes.string,
-  quantity: PropTypes.number
+  quantity: PropTypes.number,
+  buttonBar: PropTypes.node
 }
 
-export default UploadModelItem
+const enhance = compose(withState('isButtonBarVisible', 'setIsButtonBarVisible', true))
+
+export default enhance(UploadModelItem)
--- stories/component/upload-model-item.js
+++ stories/component/upload-model-item.js
@@ -1,7 +1,27 @@
 import React from 'react'
-import {storiesOf} from '@storybook/react'
+import {storiesOf, action} from '@storybook/react'
 
 import UploadModelItem from '../../src/app/component/upload-model-item'
+import Button from '../../src/app/component/button'
+import ButtonBar from '../../src/app/component/button-bar'
+
+import placeholderIcon from '../../src/asset/icon/placeholder.svg'
+
+const buttonBar = () => (
+  <ButtonBar>
+    <Button label="Button" modifiers={['tiny', 'minor']} onClick={action('click')} />
+    <Button
+      icon={placeholderIcon}
+      modifiers={['tiny', 'circular', 'minor']}
+      onClick={action('click')}
+    />
+    <Button
+      icon={placeholderIcon}
+      modifiers={['tiny', 'circular', 'minor']}
+      onClick={action('click')}
+    />
+  </ButtonBar>
+)
 
 storiesOf('Upload Model Item', module)
   .add('default', () => (
@@ -10,6 +30,7 @@ storiesOf('Upload Model Item', module)
       title="model_item_title.stl"
       subline="42 x 42 x 42 mm"
       quantity={1}
+      buttonBar={buttonBar()}
     />
   ))
   .add('no subline', () => (
@@ -17,5 +38,6 @@ storiesOf('Upload Model Item', module)
       imageSource="http://placehold.it/180x180"
       title="model_item_title.stl"
       quantity={1}
+      buttonBar={buttonBar()}
     />
   ))
