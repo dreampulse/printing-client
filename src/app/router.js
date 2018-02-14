deabba44bbea50bb3a6bce9c8c666f4166478275
Jonathan Häberle <jonathan.haeberle@gmail.com>
2018-02-14 16:51:58 +0100

    Integrate upload page

--- src/app/router.js
+++ src/app/router.js
@@ -2,21 +2,13 @@ import React from 'react'
 import {ConnectedRouter} from 'react-router-redux'
 import {Route, Switch} from 'react-router'
 
-import CartPage from './container/cart-page'
-import ModelPage from './container/model-page'
-import AddressPage from './container/address-page'
-import SuccessPage from './container/success-page'
-import ConfigurationPage from './container/configuration-page'
+import UploadPage from './container-next/upload-page'
 
 /* eslint-disable react/prop-types */
 export default ({history}) => (
   <ConnectedRouter history={history}>
     <Switch>
-      <Route component={ModelPage} path="/" exact />
-      <Route component={AddressPage} path="/address" />
-      <Route component={CartPage} path="/cart" />
-      <Route component={SuccessPage} path="/success" />
-      <Route component={ConfigurationPage} path="/configuration/:id" />
+      <Route component={UploadPage} path="/" exact />
     </Switch>
   </ConnectedRouter>
 )
