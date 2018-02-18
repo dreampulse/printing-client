ee2e15fe5484478534aa7a65bba244a6921181d7
Johannes Ewald <johannes.ewald@peerigon.com>
2018-02-19 00:05:10 +0100

    Add model-viewer component

---
+++ stories/component/model-viewer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+
+import ModelViewer from '../../src/app/component/model-viewer'
+
+const Container = ({children}) => <div style={{height: '500px'}}>{children}</div>
+
+storiesOf('Model Viewer', module)
+  .add('Without scene id', () => (
+    <Container>
+      <ModelViewer />
+    </Container>
+  ))
+  .add('With scene id', () => (
+    <Container>
+      <ModelViewer sceneId="1d4d7790-c1b5-42eb-8815-ba49349f45df" />
+    </Container>
+  ))
