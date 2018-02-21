75ca66f062ca9e3e2e4e2804149930064ed75cad
Jonathan Häberle <jonathan.haeberle@gmail.com>
2018-02-21 14:08:11 +0100

    Fix review issues

--- stories/component/magnifyable-item.js
+++ stories/component/magnifyable-item.js
@@ -6,18 +6,10 @@ import MagnifyableItem from '../../src/app/component/magnifyable-item'
 
 const Item = () => <div style={{height: '300px'}} />
 
-storiesOf('Magnifyable Item', module)
-  .add('black on grey', () => (
-    <div style={{width: '50%', backgroundColor: 'lightgrey', color: 'black'}}>
-      <MagnifyableItem ariaLabel="Magnify that item" onClick={action('click')}>
-        <Item />
-      </MagnifyableItem>
-    </div>
-  ))
-  .add('white on black', () => (
-    <div style={{width: '50%', backgroundColor: 'black', color: 'white'}}>
-      <MagnifyableItem ariaLabel="Magnify that item" onClick={action('click')}>
-        <Item />
-      </MagnifyableItem>
-    </div>
-  ))
+storiesOf('Magnifyable Item', module).add('default', () => (
+  <div style={{width: '50%', backgroundColor: 'black'}}>
+    <MagnifyableItem ariaLabel="Magnify that item" onClick={action('click')}>
+      <Item />
+    </MagnifyableItem>
+  </div>
+))
