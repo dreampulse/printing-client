8558ab16d82348801134aea693a94fa24eab255a
Jonathan Häberle <jonathan.haeberle@gmail.com>
2018-02-21 17:08:46 +0100

    Fix review issues

--- stories/component/model-viewer.js
+++ stories/component/model-viewer.js
@@ -3,16 +3,6 @@ import {storiesOf} from '@storybook/react'
 
 import ModelViewer from '../../src/app/component/model-viewer'
 
-const Container = ({children}) => <div style={{height: '500px'}}>{children}</div>
-
 storiesOf('Model Viewer', module)
-  .add('default', () => (
-    <Container>
-      <ModelViewer sceneId="1d4d7790-c1b5-42eb-8815-ba49349f45df" />
-    </Container>
-  ))
-  .add('without scene id', () => (
-    <Container>
-      <ModelViewer />
-    </Container>
-  ))
+  .add('default', () => <ModelViewer sceneId="1d4d7790-c1b5-42eb-8815-ba49349f45df" />)
+  .add('without scene id', () => <ModelViewer />)
