7fad4eff7ee75ab204692709943a1158467c07ef
Chris Ranz <git@christianranz.com>
2018-02-28 16:23:09 +0100

    Add price request v3 mock

--- src/app/lib/printing-engine.js
+++ src/app/lib/printing-engine.js
@@ -1,7 +1,9 @@
 // @flow
+import uniqueId from 'lodash/uniqueId'
+import flattenDeep from 'lodash/flattenDeep'
 
 import type {Dispatch} from 'redux'
-import type {BackendModel, Action} from '../type-next'
+import type {BackendModel, Action, BackendQuote} from '../type-next'
 
 let uploadCounter = -1
 
@@ -34,5 +36,73 @@ export const uploadModel = (
     fileUnit: 'mm',
     area: 42,
     volume: 23
+  })
+}
+
+type PriceRequestModel = {
+  id: string,
+  quantity: number
+}
+
+type PriceRequestResponse = {
+  priceId: string
+}
+
+const priceRequests = {}
+
+export const requestPrice = (
+  currency: string,
+  models: [PriceRequestModel],
+  materialConfigIds: [number]
+): Promise<PriceRequestResponse> => {
+  const priceId = uniqueId('price-id-')
+  priceRequests[priceId] = {
+    currency,
+    requestCount: 0,
+    models,
+    materialConfigIds
+  }
+
+  return Promise.resolve({
+    priceId
+  })
+}
+
+type PrintingServicePriceState = {
+  [printingServiceName: string]: boolean
+}
+
+type RequestPriceIdResponse = {
+  printingServiceComplete: PrintingServicePriceState,
+  quotes: Array<BackendQuote>
+}
+
+export const requestPriceId = (priceId: string): Promise<RequestPriceIdResponse> => {
+  const printingServiceComplete = {
+    imaterialize: priceRequests[priceId].requestCount >= 0,
+    sculpteo: priceRequests[priceId].requestCount >= 1,
+    shapeways: priceRequests[priceId].requestCount >= 2
+  }
+
+  priceRequests[priceId].requestCount++
+
+  const quotes = flattenDeep(
+    priceRequests[priceId].materialConfigIds.map(materialConfigId =>
+      priceRequests[priceId].models.map(model => ({
+        quoteId: uniqueId('quote-id-'),
+        vendorId: ['sculpteo', 'imaterialize', 'shapeways'][Math.floor(Math.random() * 3)],
+        modelId: model.id,
+        materialConfigId,
+        price: Math.floor(Math.random() * 1000),
+        quantity: model.quantity,
+        currency: priceRequests[priceId].currency,
+        isPrintable: true
+      }))
+    )
+  )
+
+  return Promise.resolve({
+    printingServiceComplete,
+    quotes
   })
 }
