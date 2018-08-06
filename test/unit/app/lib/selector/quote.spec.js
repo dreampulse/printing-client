f8a83aa9f5fbeb511fd0c9a3a75c4d9dd47c5a79
Mario Volke <mv@codastic.com>
2018-08-06 16:14:29 +0200

    Calculate shipping in material page based on current cart

--- test/unit/app/lib/selector/quote.spec.js
+++ test/unit/app/lib/selector/quote.spec.js
@@ -1,7 +1,8 @@
 import {
   isQuotePollingDone,
   selectQuotePollingProgress,
-  selectQuotes
+  selectQuotes,
+  selectUsedShippingIds
 } from '../../../../../src/app/lib/selector/quote'
 
 describe('isQuotePollingDone()', () => {
@@ -74,5 +75,96 @@ describe('selectQuotes()', () => {
     }
 
     expect(selectQuotes(state), 'to equal', ['some-quote-1', 'some-quote-2'])
+  })
+})
+
+describe('selectUsedShippingIds()', () => {
+  it('returns all shipping ids', () => {
+    const state = {
+      core: {
+        modelConfigs: [
+          {
+            id: 'config-1',
+            type: 'UPLOADED',
+            shippingId: 'shipping-1'
+          },
+          {
+            id: 'config-2',
+            type: 'UPLOADED',
+            shippingId: 'shipping-2'
+          }
+        ]
+      }
+    }
+
+    expect(selectUsedShippingIds(state), 'to equal', ['shipping-1', 'shipping-2'])
+  })
+
+  it('removes empty values from return list', () => {
+    const state = {
+      core: {
+        modelConfigs: [
+          {
+            id: 'config-1',
+            type: 'UPLOADED',
+            shippingId: 'shipping-1'
+          },
+          {
+            id: 'config-2',
+            type: 'UPLOADED',
+            shippingId: null
+          },
+          {
+            id: 'config-3',
+            type: 'UPLOADING',
+            shippingId: null
+          }
+        ]
+      }
+    }
+
+    expect(selectUsedShippingIds(state), 'to equal', ['shipping-1'])
+  })
+
+  it('removes duplicates from return list', () => {
+    const state = {
+      core: {
+        modelConfigs: [
+          {
+            id: 'config-1',
+            type: 'UPLOADED',
+            shippingId: 'shipping-1'
+          },
+          {
+            id: 'config-2',
+            type: 'UPLOADED',
+            shippingId: 'shipping-1'
+          }
+        ]
+      }
+    }
+
+    expect(selectUsedShippingIds(state), 'to equal', ['shipping-1'])
+  })
+
+  it('excludes given model config ids', () => {
+    const state = {
+      core: {
+        modelConfigs: [
+          {
+            id: 'config-1',
+            type: 'UPLOADED',
+            shippingId: 'shipping-1'
+          },
+          {
+            id: 'config-2',
+            type: 'UPLOADED',
+            shippingId: 'shipping-2'
+          }
+        ]
+      }
+    }
+
+    expect(selectUsedShippingIds(state, ['config-2']), 'to equal', ['shipping-1'])
   })
 })
