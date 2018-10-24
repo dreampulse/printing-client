43688350461655a214695f44555736be4ccd2ebc
Henry Pötzl <henry@all3dp.com>
2018-10-24 11:46:54 +0200

    Do not keep single 'no' decision in pick-unit modal

--- src/app/reducer/core.js
+++ src/app/reducer/core.js
@@ -203,7 +203,7 @@ const updateUnit = (state, action) => ({
 
 const updateUseSameMaterial = (state, action) => ({
   ...state,
-  useSameMaterial: state.useSameMaterial && action.payload
+  useSameMaterial: action.payload
 })
 
 const updateCurrency = (state, action) => {
