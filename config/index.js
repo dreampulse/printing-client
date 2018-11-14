855c22ed000b16e23dee1b38218b34796f7acf38
Jonathan Häberle <jonathan.haeberle@gmail.com>
2018-11-14 10:33:35 +0100

    Remove cypress config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,7 @@
 import developmentSettings from './development'
 import productionSettings from './production'
-import cypressSettings from './cypress'
 
 function chooseConfig() {
-  if (process.env.NODE_ENV === 'cypress') {
-    return cypressSettings
-  }
   if (process.env.NODE_ENV === 'production') {
     return productionSettings
   }
