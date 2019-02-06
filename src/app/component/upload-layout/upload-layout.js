90a109baf88cffa7f5d56f35fc8de5c628b98056
Chris Ranz <git@christianranz.com>
2019-02-06 14:11:41 +0100

    Add upload layout component

---
+++ src/app/component/upload-layout/upload-layout.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import propTypes from '../../prop-types'
+import buildClassName from '../../lib/class-names'
+
+const UploadLayout = ({classNames, children, header, stickyFooter, footer}) => (
+  <div className={buildClassName('UploadLayout', {}, classNames)}>
+    <main className="UploadLayout__main">
+      <div className="UploadLayout__header">{header}</div>
+      <div className="UploadLayout__content">{children}</div>
+      <div className="UploadLayout__stickyFooter">{stickyFooter}</div>
+    </main>
+    <aside className="UploadLayout__footer">{footer}</aside>
+  </div>
+)
+
+UploadLayout.propTypes = {
+  ...propTypes.component,
+  children: PropTypes.node.isRequired,
+  header: PropTypes.node.isRequired,
+  footer: PropTypes.node.isRequired,
+  stickyFooter: PropTypes.node.isRequired
+}
+
+export default UploadLayout
