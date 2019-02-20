500a6c642b6e4d194d51cdac178456e07eea2efc
Chris Ranz <git@christianranz.com>
2019-02-20 17:33:42 +0100

    Fix PR issues

--- src/app/component/model-upload-list/model-upload-list.js
+++ src/app/component/model-upload-list/model-upload-list.js
@@ -4,21 +4,18 @@ import React from 'react'
 import propTypes from '../../lib/prop-types'
 import buildClassName from '../../lib/build-class-name'
 
-const ModelUploadList = ({classNames, modifiers, children, uploadArea}) => {
-  const hasModels = React.Children.count(children) > 0
-  return (
-    <div className={buildClassName('ModelUploadList', [{hasModels}, modifiers], classNames)}>
-      <ul className="ModelUploadList__items">
-        <li className="ModelUploadList__uploadArea">{uploadArea}</li>
-        {React.Children.map(children, (child, index) => (
-          <li key={`model-item-${index}`} className="ModelUploadList__item">
-            {child}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+const ModelUploadList = ({classNames, modifiers, children, uploadArea}) => (
+  <div className={buildClassName('ModelUploadList', [modifiers], classNames)}>
+    <ul className="ModelUploadList__items">
+      <li className="ModelUploadList__uploadArea">{uploadArea}</li>
+      {React.Children.map(children, (child, index) => (
+        <li key={`model-item-${index}`} className="ModelUploadList__item">
+          {child}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
 
 ModelUploadList.propTypes = {
   ...propTypes.component,
