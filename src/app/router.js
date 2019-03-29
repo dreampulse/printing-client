b2040f3c7b3072946059bad9256bbdb1a1b0b084
Jonathan Häberle <dreampulse@users.noreply.github.com>
2019-03-29 12:12:03 +0100

    Revert "Revert "Add order status page""

--- src/app/router.js
+++ src/app/router.js
@@ -2,23 +2,15 @@ import React from 'react'
 import {ConnectedRouter} from 'react-router-redux'
 import {Route, Switch} from 'react-router'
 
-import UploadPage from './container/upload-page'
-import EditMaterialPage from './container/edit-material-page'
-import CartPage from './container/cart-page'
-import ReviewOrderPage from './container/review-order-page'
-import SuccessPage from './container/success-page'
-import ConfigurationPage from './container/configuration-page'
+import App from './container/app'
+import OrderStatusPage from './container/order-status-page'
 
 /* eslint-disable react/prop-types */
 export default ({history}) => (
   <ConnectedRouter history={history}>
     <Switch>
-      <Route component={UploadPage} path="/" exact />
-      <Route component={EditMaterialPage} path="/material" exact />
-      <Route component={CartPage} path="/cart" exact />
-      <Route component={ReviewOrderPage} path="/review-order" exact />
-      <Route component={SuccessPage} path="/success" exact />
-      <Route component={ConfigurationPage} path="/configuration/:id" exact />
+      <Route component={OrderStatusPage} path="/order-status/:id" exact />
+      <Route component={App} path="/" />
     </Switch>
   </ConnectedRouter>
 )
