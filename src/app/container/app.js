6a4e0e88fb2852e4b56264bdb56c2423e84d3b0e
Chris Ranz <git@christianranz.com>
2019-04-17 14:23:57 +0200

    Fix App bootstrap

--- src/app/container/app.js
+++ src/app/container/app.js
@@ -7,19 +7,22 @@ import lifecycle from 'recompose/lifecycle'
 import * as coreActions from '../action/core'
 import {getFeatureFlags, getUrlParams} from '../lib/url'
 import {removeBootsplash} from '../service/bootsplash'
-import Modal from './modal'
 
 import UploadPage from './upload-page'
+import MaterialPage from './material-page'
 import EditMaterialPage from './edit-material-page'
 import CartPage from './cart-page'
 import ReviewOrderPage from './review-order-page'
 import SuccessPage from './success-page'
 
+import Modal from './modal'
+
 const App = () => (
   <>
     <Switch>
       <Route component={UploadPage} path="/" exact />
-      <Route component={EditMaterialPage} path="/material" exact />
+      <Route component={MaterialPage} path="/material" exact />
+      <Route component={EditMaterialPage} path="/material/edit" exact />
       <Route component={CartPage} path="/cart" exact />
       <Route component={ReviewOrderPage} path="/review-order" exact />
       <Route component={SuccessPage} path="/success" exact />
