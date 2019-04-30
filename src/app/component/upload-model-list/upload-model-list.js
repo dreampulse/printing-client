a5782f2539198706ef4b847c769606244cec96ba
Jonathan Häberle <jonathan.haeberle@gmail.com>
2019-04-30 16:38:55 +0200

    Current progress

--- src/app/component/upload-model-list/upload-model-list.js
+++ src/app/component/upload-model-list/upload-model-list.js
@@ -4,7 +4,11 @@ import {useTransition, animated} from 'react-spring'
 
 import propTypes from '../../prop-types'
 import cn from '../../lib/class-names'
-import {getIndexOfRemovedElement, getIndexOfElement} from '../../lib/index-position'
+import {
+  getIndexOfRemovedElement,
+  getIndexOfElement,
+  getRemovedElements
+} from '../../lib/index-position'
 
 import usePrevious from '../../hook/use-previous'
 
@@ -14,39 +18,54 @@ const STACK_HEIGHT = 10
 const UploadModelList = ({classNames, children}) => {
   const childrenArray = Children.toArray(children)
   const previousChildren = usePrevious(children)
+  const leavingChildren = getRemovedElements(Children.toArray(previousChildren), childrenArray)
 
-  // todo z-index
-
-  const transitions = useTransition(children, item => item.key, {
+  const transitions = useTransition(childrenArray, item => item.key, {
     from: item => ({
-      transform: `translate(0px,${getIndexOfElement(children, item.key) *
-        UPLOAD_MODEL_ITEM_HEIGHT}px)`,
-      opacity: 1
+      transform: `translate(0px, ${getIndexOfElement(childrenArray, item.key) *
+        UPLOAD_MODEL_ITEM_HEIGHT}px)`
     }),
     enter: item => ({
-      transform: `translate(0px,${getIndexOfElement(children, item.key) *
+      transform: `translate(0px, ${getIndexOfElement(childrenArray, item.key) *
         UPLOAD_MODEL_ITEM_HEIGHT}px)`
     }),
 
-    leave: item => [
-      {
-        transform: `translate(0px,${getIndexOfRemovedElement(previousChildren, children, item.key) *
-          STACK_HEIGHT}px)`,
-        zIndex:
-          childrenArray.length -
-          getIndexOfRemovedElement(previousChildren, children, item.key) +
-          100
-      }
-    ],
+    leave: item => {
+      const elementIndex = getIndexOfElement(leavingChildren, item.key)
 
+      // TODO: use interpolation for translate
+      const y = elementIndex !== 0 ? `${elementIndex * STACK_HEIGHT}px` : '0'
+
+      return {
+        transform: `translate(0px, ${y})`,
+        zIndex: leavingChildren.length - elementIndex + 100
+      }
+    },
     update: item => async next => {
-      await new Promise(resolve => setTimeout(resolve, 2000))
+      await new Promise(resolve => setTimeout(resolve, 1000))
       await next({
-        transform: `translate(0px,${getIndexOfElement(children, item.key) *
+        transform: `translate(0px, ${getIndexOfElement(childrenArray, item.key) *
           UPLOAD_MODEL_ITEM_HEIGHT}px)`
       })
     },
-    duration: 2000
+    duration: 500
+  })
+
+  const leavingTransitions = useTransition(leavingChildren, item => item.key, {
+    from: item => ({
+      transform: `translate(0px,${getIndexOfElement(leavingChildren, item.key) * STACK_HEIGHT}px)`,
+      zIndex: leavingChildren.length - getIndexOfElement(leavingChildren, item.key) + 200,
+      opacity: 0
+    }),
+    enter: _item => async next => {
+      await new Promise(resolve => setTimeout(resolve, 500))
+      await next({opacity: 1})
+      await next({
+        transform: `translate(600px,-200px)`,
+        opacity: 0
+      })
+    },
+    duration: 500
   })
 
   return (
@@ -55,6 +74,11 @@ const UploadModelList = ({classNames, children}) => {
       style={{height: `${childrenArray.length * UPLOAD_MODEL_ITEM_HEIGHT}px`}}
     >
       {transitions.map(({item, props, key}) => (
+        <animated.div key={key} style={props} className="UploadModelList__card">
+          {item}
+        </animated.div>
+      ))}
+      {leavingTransitions.map(({item, props, key}) => (
         <animated.div key={key} style={props} className="UploadModelList__card">
           {item}
         </animated.div>
--- src/app/lib/index-position.ts
+++ src/app/lib/index-position.ts
@@ -1,6 +1,7 @@
 import {ReactElement} from 'react'
 import findIndex from 'lodash/findIndex'
 import difference from 'lodash/difference'
+import differenceWith from 'lodash/differenceWith'
 
 export const getIndexPositionDiff = <P>(
   prevElements: ReactElement<P>[],
@@ -32,11 +33,21 @@ export const getIndexOfRemovedElement = <P>(
   const elementKeys = elements.map(element => element.key)
 
   const elementsDiff = difference(prevElementKeys, elementKeys)
-  console.log('-- elementsDiff', elementsDiff)
-
   const diffPosition = findIndex(elementsDiff, k => k === key)
-console.log('-- diffPosition', diffPosition)
+
   if (diffPosition === -1) return null
 
   return diffPosition
+}
+
+export const getRemovedElements = <P>(
+  prevElements: ReactElement<P>[],
+  elements: ReactElement<P>[]
+): ReactElement<P>[] => {
+  const prevElementKeys = prevElements.map(element => element.key)
+  const elementKeys = elements.map(element => element.key)
+
+  const elementsDiff = difference(prevElementKeys, elementKeys)
+
+  return elementsDiff.map(key => prevElements.find(element => element.key === key))
 }
