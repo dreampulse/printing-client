49a1f908d8b8df0de416e6ad0cc0f43dd6dd0cd2
Jonathan Häberle <jonathan.haeberle@gmail.com>
2019-05-02 16:34:26 +0200

    Fix review issues

--- src/app/component/logo/logo.js
+++ src/app/component/logo/logo.js
@@ -7,7 +7,7 @@ import cn from '../../lib/class-names'
 import logoImage from '../../../asset/image/logo.svg'
 
 const Logo = ({classNames, href = '#', alt = 'Craftcloud by All3DP'}) => (
-  <a className={cn('Logo', classNames)} href={href}>
+  <a className={cn('Logo', classNames)} href={href} target="_blank" rel="noopener noreferrer">
     <img className="Logo__image" src={logoImage} alt={alt} />
   </a>
 )
