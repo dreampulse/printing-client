2b4fd17e43ef08db010589ca752d6a98b1b52554
Alex <alexander@all3dp.com>
2019-07-29 09:40:20 +0200

    Revert "TEMP: Use Stripe test key for testing on Stage. Must be reverted before merge to master"
    This reverts commit ed38de8793b85093a78737f3d197ce4c4635a081.

--- src/app/service/stripe.ts
+++ src/app/service/stripe.ts
@@ -1,5 +1,5 @@
 import config from '../../../config'
 
-const stripe = window.Stripe('pk_test_vgy9WdRy48FnhegkMYXMQXit')
+const stripe = window.Stripe(config.stripePublicKey)
 
 export const {redirectToCheckout} = stripe
