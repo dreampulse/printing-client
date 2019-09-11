0da85b057425a0255cc3e6903a5ac84316377bf0
Jonathan Häberle <jonathan.haeberle@gmail.com>
2019-09-11 11:05:58 +0200

    Update sentry logger with latest version

--- src/app/service/logging.ts
+++ src/app/service/logging.ts
@@ -1,21 +1,18 @@
-import Raven from 'raven-js'
-import createRavenMiddleware from 'raven-for-redux'
+import * as Sentry from '@sentry/browser'
+import createSentryMiddleware from 'redux-sentry-middleware'
 
 import config from '../../../config'
 
 if (process.env.NODE_ENV === 'production') {
-  Raven.config(config.ravenUrl, {
-    release: config.ravenRelease,
-    environment: process.env.NODE_ENV
-  }).install()
+  Sentry.init({dsn: config.sentryDns})
 }
 
 export const setUserContext = (userId: string) =>
-  Raven &&
-  Raven.setUserContext({
+  Sentry &&
+  Sentry.setUser({
     userId
   })
 
-export const captureException = (error: Error) => Raven && Raven.captureException(error)
+export const captureException = (error: Error) => Sentry && Sentry.captureException(error)
 
-export const ravenMiddleware = createRavenMiddleware(Raven, {})
+export const ravenMiddleware = createSentryMiddleware(Sentry, {})
