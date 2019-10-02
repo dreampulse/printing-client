4e2bcf902f8386d1622899bad268264f63ab68de
Mario Volke <mv@codastic.com>
2019-10-02 16:46:51 +0200

    Update storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,27 +2,20 @@
 // This is just the basic way to add additional webpack configurations.
 // For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
 
-const config = require('../webpack.config')
+const customConfig = require('../webpack.config')
 
-module.exports = (baseConfig, _env, _defaultConfig) => {
-  const jsxRegExpString = /\.jsx$/.toString()
+const useStyleLoader = rule => {
+  const [_, ...rest] = rule.use
+  return {...rule, use: [{loader: 'style-loader'}, ...rest]}
+}
 
-  const useStyleLoader = rule => {
-    const [_, ...rest] = rule.use
-    return {...rule, use: [{loader: 'style-loader'}, ...rest]}
-  }
+module.exports = async ({config}) => {
+  // Avoid using MiniCssExtractPlugin in production build
+  config.module.rules = customConfig.module.rules.map(rule =>
+    rule.test && rule.test.toString() === /\.scss$/.toString() ? useStyleLoader(rule) : rule
+  )
 
-  baseConfig.module.rules = [
-    ...baseConfig.module.rules.filter(
-      rule => rule.test && rule.test.toString() !== jsxRegExpString
-    ),
-    // Avoid using MiniCssExtractPlugin in production build
-    ...config.module.rules.map(rule =>
-      rule.test && rule.test.toString() === /\.scss$/.toString() ? useStyleLoader(rule) : rule
-    )
-  ]
+  config.resolve.extensions = [...config.resolve.extensions, ...customConfig.resolve.extensions]
 
-  baseConfig.resolve.extensions = [...baseConfig.resolve.extensions, ...config.resolve.extensions]
-
-  return baseConfig
+  return config
 }
