730ed60a9fe1e612e68a5fd39ee239209370e140
Mario Volke <mv@codastic.com>
2019-10-09 15:14:54 +0200

    Remove full country list

--- src/app/lib/currency.spec.js
+++ src/app/lib/currency.spec.js
@@ -1,6 +1,5 @@
 import {getValidCurrency} from './currency'
 import config from '../../../config'
-import * as currency from '../service/currency'
 
 describe('getValidCurrency()', () => {
   let sandbox
@@ -10,13 +9,8 @@ describe('getValidCurrency()', () => {
     sandbox
       .stub(config, 'currencies')
       .value([{value: 'USD'}, {value: 'EUR'}, {value: 'GBP'}, {value: 'CAD'}, {value: 'AUD'}])
-    sandbox.stub(config, 'defaultCurrencyPerCountry').value({FO: 'EUR'})
+    sandbox.stub(config, 'defaultCurrencyPerCountry').value({DE: 'EUR'})
     sandbox.stub(config, 'defaultCurrency').value('USD')
-    sandbox.stub(currency, 'currencyByCountry').value({
-      DE: 'EUR',
-      FO: 'FOO', // Unsupported currency
-      AG: 'XCD' // Unsupported currency
-    })
   })
 
   afterEach(() => {
@@ -27,9 +21,6 @@ describe('getValidCurrency()', () => {
 
   it('returns USD for unsupported currency', () =>
     expect(getValidCurrency('AG'), 'to equal', 'USD'))
-
-  it('returns EUR for unsupported currency utilizing defaultCurrencyPerCountry', () =>
-    expect(getValidCurrency('FO'), 'to equal', 'EUR'))
 
   it('returns USD for invalid input', () => expect(getValidCurrency(null), 'to equal', 'USD'))
 })
--- src/app/lib/currency.ts
+++ src/app/lib/currency.ts
@@ -1,11 +1,6 @@
 import config from '../../../config'
-import {currencyByCountry} from '../service/currency'
 
 export const getValidCurrency = (countryCode: string): string => {
-  if (config.currencies.map(currency => currency.value).includes(currencyByCountry[countryCode])) {
-    return currencyByCountry[countryCode]
-  }
-
   if ((config.defaultCurrencyPerCountry as {[countryCode: string]: string})[countryCode]) {
     return (config.defaultCurrencyPerCountry as {[countryCode: string]: string})[countryCode]
   }
